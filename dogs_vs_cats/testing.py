bccf9050ad54678d8db279c65bc422d263ecb6c5
guiBerger <gberger.eclille@gmail.com>
2016-03-20 16:03:32 -0400

    Testing updates
    Main updates :
    - Changes in "test_model" to make it work on a FuelDataset instead of an
    InMemoryDataset
    - Changes in "update_BN_params" to make it work on Graph models

--- dogs_vs_cats/testing.py
+++ dogs_vs_cats/testing.py
@@ -1,11 +1,14 @@
 __author__ = 'Guillaume'
 
 import numpy as np
-from keras.models import model_from_json, model_from_config, Sequential
+from keras.models import model_from_json, model_from_config
 from keras import backend as K
+from keras.layers.core import Flatten
+from keras.models import Graph
 import os
 import pickle
 from scipy.ndimage.filters import gaussian_filter
+from preprocessing import standardize_dataset, convert_labels, preprocess_dataset
 
 def categorical_crossentropy(ytrue, ypred, eps=1e-6):
     return -np.mean((ytrue*np.log(ypred+eps)).sum(axis=1))
@@ -50,12 +53,40 @@ def get_best_model_from_exp(path_to_experiment, subdir_names="MEM", history_file
     return model, path_model
 
 
-def test_model(model, dataset, labels, batch_size=32, return_preds=False, verbose=True):
+def test_model(model, dataset, training_params, flip=False, return_preds=False, verbose=True):
     """
     Expects a 4D dataset (N, rows, cols, channels).
     Expects binary labels.
     """
-    predictions = model.predict(np.array(dataset,"float32"), batch_size=batch_size)
+    # New epoch
+    dataset.on_new_epoch()
+    N = training_params.Ntest/training_params.test_batch_size
+    for i in range(N):
+        if verbose:
+            print "\rBatch %d over %d"%(i,N),
+        # Get next batch
+        batch,targets= dataset.get_batch()
+        # Eventually flip
+        if flip:
+            batch = np.fliplr(batch.transpose(1,2,3,0)).transpose(3,0,1,2)
+        # Preprocess
+        for mode in training_params.valid_preprocessing:
+            batch = preprocess_dataset(batch, training_params, mode)
+        # Predict
+        if type(model) is Graph:
+            pred = model.predict({"input":np.array(batch.transpose(0,3,1,2), "float32")})["output"]
+        else:
+            pred = model.predict(np.array(batch.transpose(0,3,1,2), "float32"))
+        # Accumulate preds
+        if i==0:
+            predictions = np.copy(pred)
+            labels = np.copy(convert_labels(targets))
+        else:
+            predictions = np.concatenate((predictions,pred))
+            labels = np.concatenate((labels,convert_labels(targets)))
+    if verbose:
+        print "\r"
+    # Compute accuracy
     test_loss = categorical_crossentropy(labels, predictions).mean()
     count = np.sum(np.argmax(labels, axis=1) - np.argmax(predictions, axis=1) == 0)
     score = float(count)/labels.shape[0]
@@ -63,113 +94,132 @@ def test_model(model, dataset, labels, batch_size=32, return_preds=False, verbos
         print "Accuracy = %.3f"%(score)
         print "Test Loss = %.3f"%(test_loss)
     if return_preds:
-        return score, test_loss, predictions
+        return score, test_loss, predictions, labels
     else:
         return score, test_loss
 
-def update_BN_params(model, dataset, batch_size, scale=1.0, N=100, verbose=False):
+def update_BN_params(model, dataset, scale=1.0, N=100, verbose=False):
     # Get the names of each layer
-    names_layers = [l.name for l in model.layers]
+    layers_name = [l.name for l in model.layers]
     # Find BN layers
-    check_BN = np.array([name.find('batchnormalization') for name in names_layers])
+    check_BN = np.array([name.find('batchnormalization') for name in layers_name])
     BN_positions = np.argwhere(check_BN>-1)
     BN_positions = list(BN_positions.flatten())
-    print BN_positions
+    # Get intermediate outputs
+    intermediate_outputs = K.function([model.layers[0].input],
+                                     [model.layers[bn-1].get_output(train=True) for bn in BN_positions])
     if verbose:
         print "%d BN layers found."%len(BN_positions)
     # If no BN, return
     if not BN_positions:
         return
-    for j,pos in enumerate(BN_positions):
+    means = [0 for elem in BN_positions]
+    stds = [0 for elem in BN_positions]
+    # Compute statistics over N batches
+    for i in range(N):
         if verbose:
-            print "\rUpdating BN #%d"%(j+1)
-        # Get the BN layer and the previous one
-        previous_BN_layer = model.layers[pos-1]
-        BN_layer = model.layers[pos]
-        # Get current weights (shape = [gamma, beta, mean, std])
-        weights = BN_layer.get_weights()
-        # Create a function able to get intermediate results
-        intermediate_output = K.function([model.layers[0].input],
-                                  [previous_BN_layer.get_output(train=False)])
-        # Get the shape of mean and std (taken from Keras Batch normalization code)
-        input_shape = BN_layer.input_shape
-        reduction_axes = list(range(len(input_shape)))
-        del reduction_axes[BN_layer.axis]
-        broadcast_shape = [1] * len(input_shape)
-        broadcast_shape[BN_layer.axis] = input_shape[BN_layer.axis]
-        # Initiate new mean and std
-        new_mean = np.zeros(broadcast_shape, "float32").flatten()
-        new_std = np.zeros(broadcast_shape, "float32").flatten()
-        # First, shuffle the dataset
-        count = 0.0
-        # Then, compute new mean and new std
-        for i in range(N):
-            # Batch processing (taken from Keras Batch normalization code)
-            batch = dataset.get_batch()[0]/scale
-            out = intermediate_output([batch.transpose(0,3,1,2)])[0]
-            m = mean_with_list_axis(out, reduction_axes)
-            std = mean_with_list_axis(np.square(out-m.reshape(broadcast_shape)) + BN_layer.epsilon, reduction_axes)
-            std = np.sqrt(std)
-            # Statistics accumulation
-            new_mean += m
-            new_std += std
-            count += 1.0
-            if verbose:
-                print "\r%d processed examples..."%(batch_size*i),
-        # Averaging
-        new_mean /= count
-        new_std = new_std/count # Adding eps in order to avoid 0 division
-        # Setting new weights
-        print new_mean[0]
-        print weights[2][0]
-        weights[2]=new_mean
-        print new_std[0]
-        print weights[3][0]
-        weights[3]=new_std
-        BN_layer.set_weights(np.array(weights, "float32"))
-    if verbose:
-        print "\rDone."
+            print "\rProcessing batch %d..."%(i),
+        # Get next batch
+        batch = dataset.get_batch()[0]/scale
+        out = intermediate_outputs([batch.transpose(0,3,1,2)])
+        for i,maps in enumerate(out):
+            if maps.ndim == 4:
+                maps = maps.transpose(1,0,2,3)
+                shape = [maps.shape[0], np.prod(maps.shape[1:4])]
+                maps = maps.reshape(shape)
+                means[i] += np.mean(maps, axis=1)/N
+                stds[i] += np.std(maps, axis=1)/N
+            elif maps.ndim == 2:
+                means[i] += np.mean(maps, axis=0)/N
+                stds[i] += np.std(maps, axis=0)/N
+    # Set new statistics
+    for i,bn in enumerate(BN_positions):
+        w = model.layers[bn].get_weights()
+        w[2] = np.array(means[i], "float32")
+        w[3] = np.array(stds[i], "float32")
+        model.layers[bn].set_weights(w)
 
 def softmax(input_):
     tmp = np.exp(input_)
     return tmp/tmp.sum(axis=1)
 
-def return_detection_model(model, new_shape):
-    """
-    This function adapt input shape of the model.
-    If there is "Flatten layer" it will remove every above layers.
-    """
-    # Get the config of the trained model
-    config_dict = model.get_config()
+def adapt_to_new_input(model, input_shape, old_input_shape, verbose=False):
+    old_config = model.get_config()
+    config = old_config.copy()
+    if config.has_key("nodes"):
+        return adapt_graph_to_new_input(model, input_shape, old_input_shape, verbose)
+    else:
+        return adapt_model_to_new_input(model, input_shape, old_input_shape, verbose)
+
+def adapt_model_to_new_input(model, input_shape, old_input_shape, verbose):
+    old_config = model.get_config()
+    config = old_config.copy()
     # Adapt input shape
-    config_dict['layers'][0]['input_shape'] = new_shape
-    # Check for "Flatten + Activation" :
-    # Get the names of each layer
-    names_layers = [l.name for l in model.layers]
-    # Find BN layers
-    check_flatten = np.array([name.find('flatten') for name in names_layers])
-    flatten_position = np.argwhere(check_flatten==0)
-    if flatten_position:
-        config_dict['layers']=config_dict['layers'][0:flatten_position[0]]
-    # Create a new model from this config
-    model_detect = model_from_config(config_dict)
+    config['layers'][0]['input_shape']=input_shape
+    # Adapt maxpool layer
+    pool_size = [l.pool_size[0] for l in model.layers if l.name=="maxpooling2d"]
+    global_stride = np.prod(pool_size[0:-1])
+    new_pool_size = int(pool_size[-1]+(input_shape[1]-old_input_shape[1])/global_stride)
+    if verbose:
+        print "Input shape :", input_shape
+        print "Poolsize :", pool_size[-1], "-> (%d,%d)"%(new_pool_size,new_pool_size)
+    maxpool_pos = [i for i,l in enumerate(model.layers) if l.name=="maxpooling2d"]
+    config['layers'][maxpool_pos[-1]]['pool_size']=(new_pool_size,new_pool_size)
+    # Compile model
+    new_model = model_from_config(config)
     # Set weights
-    for i,layer in enumerate(model_detect.layers):
-        layer.set_weights(model.layers[i].get_weights())
-    return model_detect
+    for i,l in enumerate(new_model.layers):
+        l.set_weights(model.layers[i].get_weights())
+    return new_model
 
-def apply_detection(imgs, model_detect, activation="softmax", blur=True, sigma=1.5):
-    # Get model output
-    preds = model_detect.predict(imgs.transpose(0,3,1,2))
-    # Apply softmax ?
-    if activation=="softmax":
-        preds = softmax(preds)
-    if blur:
-        for i,pred in enumerate(preds):
-            for j,predmap in enumerate(pred):
-                preds[i,j] = gaussian_filter(predmap, sigma)
-    return preds
+def adapt_graph_to_new_input(model, input_shape, old_input_shape, verbose=False):
+    old_config = model.get_config()
+    config = old_config.copy()
+    # Adapt input shape
+    config['input_config'][0]['input_shape'] = input_shape
+    # Get names of each layer
+    keys = model.nodes.keys()
+    # Get the flatten layer
+    flatten_keys =[k for k in keys if  type(model.nodes[k]) is Flatten]
+    # Get the name of the layer just before Flatten
+    feature_key = keys[keys.index(flatten_keys[-1])-1]
+    # Adapt pool size
+    if config['nodes'][feature_key]["name"] == "Activation":
+        # Merge case : multiple pooling size to adapt
+        parents = get_parents(feature_key, config)
+        for par in parents:
+            # Store old pool size
+            old_pool_size = config['nodes'][par]["pool_size"]
+            # Compute Global stride
+            global_stride = 1
+            iteration = par
+            count = 0
+            while iteration!='input' and count < 100:
+                count += 1
+                iteration = get_parents(iteration, config)
+                if iteration != 'input' and config['nodes'][iteration]["name"] == "MaxPooling2D":
+                    global_stride *= config['nodes'][iteration]["pool_size"][0]
+            # Compute new pool size
+            new_pool_size = int(old_pool_size[0]+(input_shape[1]-old_input_shape[1])/global_stride)
+            if verbose:
+                print "Input shape :", input_shape
+                print "Poolsize :", old_pool_size, "-> (%d,%d)"%(new_pool_size,new_pool_size)
+            config['nodes'][par]["pool_size"] = (new_pool_size,new_pool_size)
+            config['nodes'][par]["strides"] = (new_pool_size,new_pool_size)
+    else:
+        raise Exception("Not implemented")
+    new_model = model_from_config(config)
+    # Set weights
+    for i,name in enumerate(new_model.nodes.keys()):
+        new_model.nodes[name].set_weights(model.nodes[name].get_weights())
+    return new_model
 
+def get_parents(key, config):
+    parents = [elem for elem in config['node_config'] if elem['name']==key]
+    if parents[0]["input"] is not None:
+        return parents[0]["input"]
+    else:
+        return parents[0]["inputs"]
 
 def mean_with_list_axis(a, ax):
     out = np.mean(a, axis=ax[-1])

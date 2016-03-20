d0163a89b3fc3238238e678ca0d26f262f71460a
guiBerger <gberger.eclille@gmail.com>
2016-03-20 15:53:56 -0400

    New model

--- dogs_vs_cats/def_model/vggnet_with_BN_window_100.py
+++ dogs_vs_cats/def_model/vggnet_with_BN_window_100.py
@@ -2,7 +2,7 @@ __author__ = 'Guillaume'
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Flatten, Dropout, Activation
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import SGD
 from keras.regularizers import l2
@@ -15,14 +15,17 @@ def define_model(lr, momentum):
     # Create Layers
     # CONVNET
     layers = []
-    layers.append(Convolution2D(32, 3, 3, activation = "linear", input_shape=(3,150,150)))
+    layers.append(ZeroPadding2D((1, 1), input_shape=(3,150,150)))
+    layers.append(Convolution2D(32, 3, 3, activation = "linear"))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
+    layers.append(ZeroPadding2D((1, 1)))
     layers.append(Convolution2D(32, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
     layers.append(MaxPooling2D(pool_size=(3,3))) #if image is 150x150
     #layers.append(MaxPooling2D(pool_size=(2,2))) #if image is 100x100
+    layers.append(ZeroPadding2D((1, 1)))
     layers.append(Convolution2D(64, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
@@ -36,9 +39,6 @@ def define_model(lr, momentum):
     layers.append(Convolution2D(128, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
-    layers.append(Convolution2D(128, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
-    layers.append(BatchNormalization(axis=1))
-    layers.append(Activation("relu"))
     layers.append(MaxPooling2D(pool_size=(2,2)))
     layers.append(Convolution2D(256, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
@@ -46,10 +46,7 @@ def define_model(lr, momentum):
     layers.append(Convolution2D(256, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
-    layers.append(Convolution2D(256, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
-    layers.append(BatchNormalization(axis=1))
-    layers.append(Activation("relu"))
-    layers.append(MaxPooling2D(pool_size=(2,2)))
+    layers.append(MaxPooling2D(pool_size=(6,6)))
 
     # MLP
     layers.append(Flatten())
--- dogs_vs_cats/def_model/vggnet_with_BN_window_46.py
+++ dogs_vs_cats/def_model/vggnet_with_BN_window_46.py
@@ -2,10 +2,10 @@ __author__ = 'Guillaume'
 
 from keras.models import Sequential
 from keras.layers.core import Dense, Flatten, Dropout, Activation
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
+from keras.layers.convolutional import Convolution2D, MaxPooling2D, ZeroPadding2D
 from keras.layers.normalization import BatchNormalization
 from keras.optimizers import SGD
-from keras.regularizers import l2
+from keras.regularizers import l2, ActivityRegularizer
 
 
 def define_model(lr, momentum):
@@ -15,49 +15,47 @@ def define_model(lr, momentum):
     # Create Layers
     # CONVNET
     layers = []
-    layers.append(Convolution2D(32, 3, 3, activation = "linear", input_shape=(3,150,150)))
+    layers.append(ZeroPadding2D((1, 1), input_shape=(3,150,150)))
+    layers.append(Convolution2D(32, 3, 3, activation = "linear"))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
+    layers.append(ZeroPadding2D((1, 1)))
     layers.append(Convolution2D(32, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
     layers.append(MaxPooling2D(pool_size=(3,3))) #if image is 150x150
     #layers.append(MaxPooling2D(pool_size=(2,2))) #if image is 100x100
+    layers.append(ZeroPadding2D((1, 1)))
     layers.append(Convolution2D(64, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
+    layers.append(ZeroPadding2D((1, 1)))
     layers.append(Convolution2D(64, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
     layers.append(MaxPooling2D(pool_size=(2,2)))
+    layers.append(ZeroPadding2D((1, 1)))
     layers.append(Convolution2D(128, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
+    layers.append(ZeroPadding2D((1, 1)))
     layers.append(Convolution2D(128, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
-    layers.append(Convolution2D(128, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
+    layers.append(ZeroPadding2D((1, 1)))
+    layers.append(Convolution2D(128, 3, 3, activation = "linear"))#, activity_regularizer=ActivityRegularizer(l1=0.01)))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization(axis=1))
     layers.append(Activation("relu"))
-    layers.append(MaxPooling2D(pool_size=(2,2)))
-    layers.append(Convolution2D(256, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
-    layers.append(BatchNormalization(axis=1))
-    layers.append(Activation("relu"))
-    layers.append(Convolution2D(256, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
-    layers.append(BatchNormalization(axis=1))
-    layers.append(Activation("relu"))
-    layers.append(Convolution2D(256, 3, 3, activation = "linear"))#, W_regularizer=l2(0.0001)))
-    layers.append(BatchNormalization(axis=1))
-    layers.append(Activation("relu"))
-    layers.append(MaxPooling2D(pool_size=(2,2)))
+    layers.append(MaxPooling2D(pool_size=(25,25)))
 
     # MLP
     layers.append(Flatten())
-    layers.append(Dense(1024, activation="linear"))#, W_regularizer=l2(0.0001)))
+    #layers.append(Dropout(p=0.5))
+    layers.append(Dense(512, activation="linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization())
     layers.append(Activation("relu"))
     #layers.append(Dropout(p=0.4))
-    layers.append(Dense(512, activation="linear"))#, W_regularizer=l2(0.0001)))
+    layers.append(Dense(256, activation="linear"))#, W_regularizer=l2(0.0001)))
     layers.append(BatchNormalization())
     layers.append(Activation("relu"))
     #layers.append(Dropout(p=0.4))
